5f8920f89cfa66c87f5009af1e0f29ae9d5a786c
leesemin <leesemin222@gmail.com>
2023-09-06 18:04:08 +0900

    모델 적용하기

--- ch3/polls/models.py
+++ ch3/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+def validate_positive(value):
+    if value <= 0:
+        raise ValidationError("0이하는 입력되지 않습니다.")
+
+
+class pracTable(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    number = models.IntegerField(null=False)
+    nickname = models.CharField(max_length=100, null=False)
+    deposit = models.BigIntegerField(validators=[validate_positive], null=False)
+    score = models.IntegerField()
