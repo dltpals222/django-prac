ae526ad31b68e85810423f777851c6a3491dbd9e
leesemin <leesemin222@gmail.com>
2023-09-13 00:12:49 +0900

    입력은 되는데 에러처리가 필요해보인다.

--- ch3/polls/forms.py
+++ ch3/polls/forms.py
@@ -6,3 +6,16 @@ class MytableForm(forms.ModelForm):
     class Meta:
         model = mytable
         fields = ["name", "number", "nickname", "deposit", "score"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "width-15percent"}),
+            "number": forms.TextInput(attrs={"class": "width-10percent"}),
+            "nickname": forms.TextInput(attrs={"class": "width-20percent"}),
+            "deposit": forms.TextInput(attrs={"class": "width-35percent"}),
+            "score": forms.TextInput(attrs={"class": "width-20percent"}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if not all(cleaned_data.values()):
+            raise forms.ValidationError("5군데 모두 입력해 주시기 바랍니다.")
--- ch3/polls/views.py
+++ ch3/polls/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import mytable
@@ -31,50 +32,23 @@ def userManagement(request):
     return render(request, "polls/userManagement.html", tag_data)
 
 
+MytableFormSet = forms.formset_factory(MytableForm, extra=10)
+
+
 def create(request):
     if request.method == "POST":
-        form = MytableForm(request.POST)
-        if form.is_valid():
-            # 10개의 필드값 전부 가져오기
-            field_values = []
-            for i in range(10):
-                name = request.POST.get(f"name_{i}")
-                number = request.POST.get(f"number_{i}")
-                nickname = request.POST.get(f"nickname_{i}")
-                deposit = request.POST.get(f"deposit_{i}")
-                score = request.POST.get(f"score_{i}")
-                if (
-                    name != ""
-                    and number != ""
-                    and nickname != ""
-                    and deposit != ""
-                    and score != ""
-                ):
-                    obj = {
-                        "name": name,
-                        "number": number,
-                        "nickname": nickname,
-                        "deposit": deposit,
-                        "score": score,
-                    }
-                    field_values.append(obj)
-
-            # DB에 저장하기
-            for value in field_values:
-                entry = mytable(
-                    name=value["name"],
-                    number=int(value["number"]),
-                    nickname=value["nickname"],
-                    deposit=int(value["deposit"]),
-                    score=int(value["score"]),
-                )
-                entry.save()
-
+        formset = MytableFormSet(request.POST)
+        if formset.is_valid():
+            for i in formset:
+                if i.is_valid() and i.has_changed():
+                    instance = i.save()
             return HttpResponseRedirect("polls/create")
     else:
-        form = MytableForm()
+        formset = MytableFormSet()
 
-    return render(request, "polls/create.html", tag_data)
+    context = {**tag_data, "formset": formset}
+
+    return render(request, "polls/create.html", context)
 
 
 def update(request):
